4febe73805b704c2489501fb0efc500a65822dd6
Omar Hanafi <ohanafi@salesforce.com>
2022-12-05 11:14:03 +0100

    Dynamic PDF

--- src/lwc/bestPracticesLwc/bestPracticesLwc.js
+++ src/lwc/bestPracticesLwc/bestPracticesLwc.js
@@ -9,13 +9,25 @@ export default class BestPracticesLwc extends LightningElement {
     isVisible = true;
 
     activeSections = [];
+
     bestPractices;
+    playbookUrl;
+
     error;
+
+    get pdfHeight() {
+        return 'height: ' + this.playbookHeightInRem + 'rem';
+    }
 
     @wire(getBestPractices, { parentId: '$recordId' })
     wiredBestPractices({ error, data }) {
         if (data) {
-            this.bestPractices = data;
+            this.bestPractices = [...data];
+            let playbookIndex = this.bestPractices.findIndex(bp => bp.DeveloperName?.includes('SureSmile_Commercial_Playbook'));
+            if(playbookIndex != -1) {
+                this.playbookUrl = this.bestPractices[playbookIndex].BestPracticeContent__c;
+                this.bestPractices.splice(playbookIndex, 1);
+            }
         } else if (error) {
             this.error = error;
             console.log("error", error);
