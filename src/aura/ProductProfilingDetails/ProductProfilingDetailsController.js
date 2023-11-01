9b58e4e8b0339cd5031715e8537d65ec38c26678
Omar Hanafi <ohanafi@salesforce.com>
2023-11-01 14:18:57 +0100

    Set the fields: Workflow__c and WorkflowLine__c on the ProductProfiling__c record

--- src/aura/ProductProfilingDetails/ProductProfilingDetailsController.js
+++ src/aura/ProductProfilingDetails/ProductProfilingDetailsController.js
@@ -1,12 +1,18 @@
 ({
     init: function (cmp, event, helper) {
-        
+
         var productMap = {};
+        var workflowMap = {};
+        var workflowLineMap = {};
+
         var productWrapperList = cmp.get("v.selectedProductsWrapper");
-                
+
+
         for(var product in productWrapperList){
             var productId = productWrapperList[product].productId;
-            productMap[productId] = productWrapperList[product].productName;            
+            productMap[productId] = productWrapperList[product].productName;
+            workflowMap[productId] = productWrapperList[product].workflow;
+            workflowLineMap[productId] = productWrapperList[product].workflowLine;
         }
         
         console.log('Product map: ' + JSON.stringify(productMap));
@@ -17,8 +23,9 @@
         for(var prod in products){
             var prodId = products[prod].Product_Name__c;
 			console.log(productMap[prodId]);
-            var productName = productMap[prodId];
-            products[prod].productName =productName;
+            products[prod].productName = productMap[prodId];
+            products[prod].Workflow__c = workflowMap[prodId];
+            products[prod].WorkflowLine__c = workflowLineMap[prodId];
         }
         
         cmp.set('v.columns', [
